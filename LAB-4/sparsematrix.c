a74a5dbb788eeb9ebc1176fb12a9dc0864cddea3
abhijeeth004 <142534289+abhijeeth004@users.noreply.github.com>
2023-09-12 16:20:59 +0530

    Create sparsematrix.c

---
+++ LAB-4/sparsematrix.c
@@ -0,0 +1,53 @@
+int row ;
+int col;
+int sparseLen=0;
+typedef struct{
+int row;
+int col;
+int val;
+}sparseSt;
+int main()
+{
+    printf("enter rows:\n");
+    scanf("%d",&row);
+    printf("enter col:\n");
+    scanf("%d",&col);
+    printf("enter the values:\n");
+    int sparseMat[row][col];
+    for (int i=0;i<row;i++)
+    {
+        for(int j=0;j<col;j++)
+        {
+            int temp;
+            scanf("%d",&temp);
+            sparseMat[i][j]=temp;
+            if(temp!=0) sparseLen++;
+        }
+    }
+    sparseSt a[sparseLen+1];
+    a[0].row=row;
+    a[0].col=col;
+    a[0].val=sparseLen;
+    int ind=1;
+    for (int i=0;i<row;i++)
+    {
+        for(int j=0;j<col;j++)
+        {
+            int temp=sparseMat[i][j];
+            if(temp!=0)
+            {
+                a[ind].row=i;
+                a[ind].col=j;
+                a[ind].val=temp;
+                ind++;
+
+            }
+        }
+    }
+    printf("Sparse matrix struct:\n");
+    for(int i=0;i<=sparseLen;i++)
+    {
+        printf("%d %d %d\n",a[i].row,a[i].col,a[i].val);
+    }
+    return 0;
+}
